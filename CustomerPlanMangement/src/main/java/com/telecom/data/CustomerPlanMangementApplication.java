5a44faad4bd2a71532526ddfaa325e38399605fa
Deepak <132191684+Deepak-dalai@users.noreply.github.com>
2024-06-07 13:23:17 +0530

    Add files via upload

---
+++ CustomerPlanMangement/src/main/java/com/telecom/data/CustomerPlanMangementApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPlanMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPlanMangementApplication.class, args);
+	}
+
+}
---
+++ CustomerPlanMangement/src/test/java/com/telecom/data/CustomerPlanMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPlanMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
