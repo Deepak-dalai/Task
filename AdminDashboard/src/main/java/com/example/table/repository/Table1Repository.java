05442ca40388794e5b3a0023f6599c97dab0ea32
Deepak <132191684+Deepak-dalai@users.noreply.github.com>
2024-06-07 13:31:24 +0530

    Add files via upload

---
+++ AdminDashboard/src/main/java/com/example/table/repository/Table1Repository.java
@@ -0,0 +1,9 @@
+package com.example.table.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.table.entity.Table1;
+
+public interface Table1Repository extends JpaRepository<Table1, Long> {
+
+}
---
+++ AdminDashboard/src/main/java/com/example/table/repository/Table2Repository.java
@@ -0,0 +1,9 @@
+package com.example.table.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.table.entity.Table2;
+
+public interface Table2Repository extends JpaRepository<Table2, Long> {
+
+}
