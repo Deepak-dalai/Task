aa0d62bea90908bb71ac87f799924d5533a3c5dd
Deepak <132191684+Deepak-dalai@users.noreply.github.com>
2024-06-20 11:00:26 +0530

    Add files via upload

---
+++ Automated_email_sender/src/main/java/com/example/email/AutomatedEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedEmailSenderApplication.class, args);
+	}
+
+}
