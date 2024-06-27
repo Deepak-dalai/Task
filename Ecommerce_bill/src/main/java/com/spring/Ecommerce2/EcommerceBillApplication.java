67e86319cd8c4f1a6d07313592f09273c4b4cba3
Deepak <132191684+Deepak-dalai@users.noreply.github.com>
2024-06-27 15:22:24 +0530

    Add files via upload

---
+++ Ecommerce_bill/src/main/java/com/spring/Ecommerce2/EcommerceBillApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Ecommerce2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBillApplication.class, args);
+	}
+
+}
---
+++ Ecommerce_bill/src/test/java/com/spring/Ecommerce2/EcommerceBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Ecommerce2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
